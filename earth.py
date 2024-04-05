b819f50b4a081e490650ea9ea7f68982a79a524e
Matias Ponce <matiaseponce@gmail.com>
2024-04-05 11:52:42 -0300

    Update earth.py

--- earth.py
+++ earth.py
@@ -1,3 +1,7 @@
 def earth():
     x = "Bangladesh"
     y = "Barbados"
+    condicion = x < y
+    condicion_2 = not condicion
+    print(f"The result of {x} comes first in the dictionary than {y} is { condicion }.")
+    print(f"The result of {y} comes first in the dictionary than {x} is {not condicion}.")
