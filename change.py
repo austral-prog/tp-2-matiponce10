fe58412978e2888cf82b5891837333136c8a2ba1
Matias Ponce <matiaseponce@gmail.com>
2024-04-05 12:02:28 -0300

    Update change.py

--- change.py
+++ change.py
@@ -1,3 +1,16 @@
 def change():
     expense = 23.75
     money = 100
+    print("Ingresar gasto:")
+    print(expense)
+    print("Dinero recibido")
+    print(money)
+    print("")
+    print("Vuelto")
+    print("")
+    diferencia = money - expense
+    diferencia_entero = int(diferencia)
+    print("Pesos:")
+    print(diferencia_entero)
+    print("Centavos:")
+    print(int((diferencia % 1) * 100)) # 25
